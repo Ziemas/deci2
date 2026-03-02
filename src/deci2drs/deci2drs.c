5f758a8ac93b5c2ed1b83896314d715029842b45
Ziemas <ziemas@ziemas.se>
2026-03-02 07:21:50 +0100

    drs: migrate data

--- src/deci2drs/deci2drs.c
+++ src/deci2drs/deci2drs.c
@@ -1,7 +1,84 @@
 #include "common.h"
 
-#define IRQ_STAT 0xbf801070
-#define DMA_DICR 0xbf8010f4
+#include <loadcore.h>
+
+extern char D_000010B0[];
+ModuleInfo Module = { D_000010B0, 0x103 };
+
+extern char D_00001138[];
+extern char D_0000112C[];
+extern char D_00001124[];
+extern char D_00001118[];
+extern char D_0000110C[];
+extern char D_00001100[];
+extern char D_000010F8[];
+extern char D_000010F0[];
+extern char D_000010E8[];
+extern char D_000010DC[];
+extern char D_000010D4[];
+extern char D_000010CC[];
+
+int func_0000068C();
+int func_000006CC();
+int func_0000079C();
+int func_000007E0();
+int func_00000828();
+int func_00000940();
+int func_00000B80();
+int func_00000CAC();
+int func_00000CEC();
+int func_00000D58();
+int func_00000D98();
+int func_00000E04();
+int func_00000E0C();
+
+void *drs_iface_func[] = {
+	func_0000068C,
+	func_000006CC,
+	func_0000079C,
+	func_000007E0,
+	func_00000828,
+	func_00000940,
+
+	func_00000B80,
+	func_00000CAC,
+	func_00000CEC,
+	func_00000D58,
+	func_00000D98,
+	func_00000E04,
+	func_00000E0C,
+};
+
+const char *drs_iface_func_name[] = {
+	D_00001138,
+	D_0000112C,
+	D_00001124,
+	D_00001118,
+	D_0000110C,
+	D_00001100,
+	D_000010F8,
+	D_000010F0,
+	D_000010E8,
+	D_000010DC,
+	D_000010D4,
+	D_000010CC,
+};
+
+struct drv_sif {
+	int iface;
+	int flag;
+	int field_8;
+	int field_c;
+	int field_10;
+	int field_14;
+	int field_18;
+	int field_1c;
+	int field_20;
+	int field_24;
+	int field_28;
+	int field_2c;
+	int field_30;
+};
 
 INCLUDE_ASM("asm/deci2drs/nonmatchings/deci2drs", start);
 
