c1472f084fa93ffed6d4eaf26fbf6721a795b80e
Ziemas <ziemas@ziemas.se>
2026-03-16 14:06:02 +0100

    add configs for the other modules

---
+++ src/deci2load/deci2load.c
@@ -0,0 +1,56 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", start);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000098);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000000EC);
+
+INCLUDE_RODATA("asm/deci2load/nonmatchings/deci2load", D_00001810);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000194);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_0000042C);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000430);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000004E8);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000518);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000005C4);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000668);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000714);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000750);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000007A4);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000007DC);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000D5C);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00000FFC);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00001094);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000010C0);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00001150);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00001160);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00001198);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_000011F8);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00001220);
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_00001458);
+
+void func_00001624(void) {
+}
+
+INCLUDE_ASM("asm/deci2load/nonmatchings/deci2load", func_0000162C);
